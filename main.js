7ad53f3ddf57bf4b6c74c0ba2c79b65946784f33
akramjavith <105336866+akramjavith@users.noreply.github.com>
2022-06-10 15:22:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const game = () => {
  
                 const movesLeft = document.querySelector('.movesleft');
                 moves++;
-                movesLeft.innerText = `Moves Left: ${10-moves}`;
+                movesLeft.innerText = `Moves Left: ${5-moves}`;
                  
  
                 const choiceNumber = Math.floor(Math.random()*3);
@@ -31,7 +31,7 @@ const game = () => {
                 winner(this.innerText,computerChoice)
                  
                 // Calling gameOver function after 10 moves
-                if(moves == 10){
+                if(moves == 5){
                     gameOver(playerOptions,movesLeft);
                 }
             })
